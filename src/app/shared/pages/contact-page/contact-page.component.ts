18c0738849f43a90dd7c6916bc46a2b5e01ff1ab
karlux <karlux.sistemas@gmail.com>
2023-08-19 21:05:30 -0300

    feat: add shared module with routing and components to test

---
+++ src/app/shared/pages/contact-page/contact-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-contact',
+  templateUrl: './contact-page.component.html',
+  styles: [],
+})
+export class ContactPageComponent {}
